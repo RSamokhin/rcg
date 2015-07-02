249e6fdd840ddbdfdb43995c288dbea74c2683ff
rsamokhin <poshliemail@googlemail.com>
2015-07-02 13:47:23 +0300

    news obj changed

--- models/news.js
+++ models/news.js
@@ -5,24 +5,17 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.INTEGER,
             field: 'n_id'
         },
-        title: DataTypes.STRING,
         authorId: {
             type: DataTypes.STRING,
             field: 'author_id'
         },
-        text: DataTypes.STRING,
+        title: DataTypes.STRING,
+        picture: DataTypes.STRING,
         shortText: {
             type: DataTypes.STRING,
             field: 'short_text'
         },
-        isVacancy: {
-            type: DataTypes.BOOLEAN,
-            field: 'is_vacancy'
-        },
-        dateCreated: {
-            type: DataTypes.DATE,
-            field: 'datecreated'
-        },
+        text: DataTypes.STRING,
         datePublished: {
             type: DataTypes.DATE,
             field: 'datepubliched'
