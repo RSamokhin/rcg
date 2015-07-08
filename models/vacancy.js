a05073fe8224dd67374bbb26d6741ea8ce065d37
rsamokhin <poshliemail@googlemail.com>
2015-07-08 17:19:32 +0300

    HODOR

--- models/vacancy.js
+++ models/vacancy.js
@@ -1,10 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
     var Vacancy = sequelize.define("Vacancy", {
-        newsId: {
-            type: DataTypes.INTEGER,
-            field: 'news_id',
-            primaryKey: true
-        },
         isMale: {
             type: DataTypes.BOOLEAN,
             field: 'is_male'
