e17e499a46f70f19e72af93b17d36db92f765cc3
rsamokhin <poshliemail@googlemail.com>
2015-08-27 22:09:14 +0300

    news update

--- web/src/js/partials/init.js
+++ web/src/js/partials/init.js
@@ -12,6 +12,9 @@ window.Handlers = {
             $.ajax({
                 url: url,
                 success: function (data) {
+                    data.forEach(function (d) {
+                        d.data = encodeURI(JSON.stringify(d));
+                    });
                     $('[data-append-to='+aim+']').html('');
                     $('#'+templateId).tmpl(data).appendTo('[data-append-to='+aim+']');
                 }
@@ -23,8 +26,28 @@ window.Handlers = {
                 $field = $(this).parent().find('input, textarea');
             if ($button.hasClass('m-button-editable')) {
                 $button.removeClass('m-button-editable').addClass('m-button-saveable');
+                $field.attr("readonly", false).focus();
             } else {
-                $button.addClass('m-button-editable').removeClass('m-button-saveable');
+                var fullData = JSON.parse(decodeURI($field.closest('tr').attr('data-parsed'))),
+                    newValue;
+                switch ($field.prop("tagName")) {
+                    case 'INPUT':
+                        newValue = $field.val();
+                        break;
+                    case 'TEXTAREA':
+                        newValue = $field.val();
+                        break;
+                }
+                fullData[$field.attr('data-field')] = newValue;
+                $.ajax({
+                    type: "POST",
+                    url: $field.closest('table').attr('data-update-url').replace(':key', fullData[$field.closest('table').attr('data-update-key')]),
+                    data:fullData,
+                    success: function () {
+                        $button.addClass('m-button-editable').removeClass('m-button-saveable');
+                        $field.attr("readonly", true);
+                    }
+                });
             }
         }
     }
