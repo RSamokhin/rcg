76fb7aee6a30cacc46d7d3c61d58a93fe60bd975
RSamokhin <poshliemail@googlemail.com>
2015-08-28 21:46:57 +0300

    unused bower removed

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -3,7 +3,6 @@
  */
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
-//= ../../bower_components/bs-confirmation/bootstrap-confirmation.js
 //= ../../bower_components/jquery-tmpl/jquery.tmpl.min.js
 
 /*
