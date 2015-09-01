139256d89836dc89f10e3198708821511068ee77
rsamokhin <poshliemail@googlemail.com>
2015-09-01 13:49:46 +0300

    merge w rcg-20 #20 fix

--- controllers/images.js
+++ controllers/images.js
@@ -5,34 +5,25 @@ var parse = require('co-busboy');
 var allowed = ['jpg', 'png', 'gif'];
 
 module.exports.add = function *(next){
-   // if (this.is('image/*')) {
-        var parts = parse(this);
-        var part;
-        while (part = yield parts) {
+    var parts = parse(this);
+    var part;
+    while (part = yield parts) {
 
-            var ext = path.extname(part.filename).slice(1);
-            if (allowed.indexOf(ext) === -1)
-            {
-                this.body = {
-                    status: 'ERROR',
-                    error : 'unsupported type'
-                };
-                return;
-            }
-            var stream = fs.createWriteStream(path.join('images', guid()) + '.' + part.filename.split('.').slice(-1).toString());
-            part.pipe(stream);
+        var ext = path.extname(part.filename).slice(1);
+        if (allowed.indexOf(ext) === -1) {
             this.body = {
-                status: 'OK',
-                path : '/' + stream.path
+                status: 'ERROR',
+                error : 'unsupported type'
             };
+            return;
         }
- /*   } else {
-        this.body =this {
-            status: 'ERROR',
-            error : 'unsupported type'
+        var stream = fs.createWriteStream(path.join('images', guid()) + '.' + part.filename.split('.').slice(-1).toString());
+        part.pipe(stream);
+        this.body = {
+            status: 'OK',
+            path : '/' + stream.path
         };
     }
-     */
     function guid() {
         function s4() {
             return Math.floor((1 + Math.random()) * 0x10000)
@@ -44,7 +35,9 @@ module.exports.add = function *(next){
     }
 };
 module.exports.get = function *(fname){
-    var fs = require('fs');
-    var readStream = fs.createReadStream('images/' + fname);
-    this.body = readStream;
+    if (/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(fname.split('.').slice(-2,-1))) {
+        var fs = require('fs');
+        var readStream = fs.createReadStream('images/' + fname);
+        this.body = readStream;
+    }
 };
