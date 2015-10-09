1920b8002d1822c93bcbea9eaa5c835a181e2eea
root <root@agency.agency.a10.internal.cloudapp.net>
2015-10-09 19:50:19 +0000

    Production: false

--- apns.js
+++ apns.js
@@ -8,17 +8,31 @@ var cfg = require('./config');
 
 var options = {
     cert: cfg.apns.cert,
-    key: cfg.apns.key
+    key: cfg.apns.key,
+    production: false 
 };
 
 var pushToDevices = function(message, opt_payload)
 {
+//    console.log("PUsh", message);
     co(function * (){
         var devices = yield models.Devices.findAll();
         if (!devices.length)
             return;
 
         var apnConnection = new apn.Connection(options);
+
+ //       var log = function(event, data) {
+ //           console.log(arguments)
+ //       };
+
+ //       apnConnection.on('error', log.bind(null, 'error'));
+ //       apnConnection.on('socketError', log.bind(null, 'socketError'));
+ //       apnConnection.on('transmitted', log.bind(null, 'transmitted'));
+ //       apnConnection.on('completed', log.bind(null, 'completed'));
+ //       apnConnection.on('transmissionError', log.bind(null, 'transmissionError'));
+
+
         var note = new apn.Notification();
         note.alert = message;
         if (opt_payload !== undefined)
